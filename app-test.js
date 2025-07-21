24ffda04c2e29f69453561375b78e9d47deaae47
sreni <33428674+sreni@users.noreply.github.com>
2025-07-22 00:49:05 +0530

    uncommneted test cases

--- app-test.js
+++ app-test.js
@@ -8,7 +8,7 @@ let chaiHttp = require("chai-http");
 chai.should();
 chai.use(chaiHttp); 
 
-/*describe('Planets API Suite', () => {
+describe('Planets API Suite', () => {
 
     describe('Fetching Planet Details', () => {
         it('it should fetch a planet named Mercury', (done) => {
@@ -147,7 +147,7 @@ chai.use(chaiHttp);
 
 
     });        
-});*/
+});
 
 //Use below test case to achieve coverage
 describe('Testing Other Endpoints', () => {
